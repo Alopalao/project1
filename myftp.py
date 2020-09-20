c0b00176dcb6db0ad945c171f01cb879a27935ba
Aldo Ortega <alopalao@gmail.com>
2020-09-20 14:41:28 -0400

    Function specified

--- myftp.py
+++ myftp.py
@@ -1,5 +1,34 @@
 import socket
-import subprocess
+
+def command_ls(mysocket):
+    request = 'PASV\r\n'
+    mysocket.send(str.encode(request))
+
+    response = bytes.decode(mysocket.recv(1024))
+    #print(response)
+    word = 0
+    number1 = ''
+    number2 = ''
+    for i in response:
+        if(i == ','):
+            word += 1
+        elif(i == ')'):
+            break
+        elif(word == 4):
+            number1 += i
+        elif(word == 5):
+            number2 += i
+    Port = int((int(number1)*256)+int(number2))
+
+    mysocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysocket2.connect(('inet.cs.fiu.edu', Port))
+
+    request = 'NLST\r\n'
+    mysocket.send(str.encode(request))
+    
+    response = bytes.decode(mysocket2.recv(1024))
+    print(response)
+
 
 request = ''
 response = ''
@@ -9,50 +38,28 @@ mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(('inet.cs.fiu.edu', 21))
 response = bytes.decode(mysocket.recv(1024))
 print(response)
+
 command = input ('USER: ')
 request = 'User ' + command + '\n'
 mysocket.send(str.encode(request))
+
 response = bytes.decode(mysocket.recv(1024))
 print(response)
+
 command = input('Password: ')
 request = 'Pass ' + command + '\n'
 mysocket.send(str.encode(request))
-response = bytes.decode(mysocket.recv(1024))
-print(response)
-#while(command != 'quit'):
-command = input('myftp> ')
-request = 'PASV\r\n'
-mysocket.send(str.encode(request))
+
 response = bytes.decode(mysocket.recv(1024))
 print(response)
 
-word = 0
-number1 = ''
-number2 = ''
-for i in response:
-    if(i == ','):
-        word += 1
-    elif(i == ')'):
-        break
-    elif(word == 4):
-        number1 += i
-    elif(word == 5):
-        number2 += i
+while(command != 'quit'):
+    command = input('myftp> ')
 
-print("Number 1: ", number1)
-print("number 2: ", number2)
-total = int((int(number1)*256)+int(number2))
-print("Total = ", total)
+    if(command == 'ls'):
+        command_ls(mysocket)
 
-mysocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysocket2.connect(('inet.cs.fiu.edu', total))
-
-request = 'NLST\r\n'
-mysocket.send(str.encode(request))
-response = bytes.decode(mysocket2.recv(1024))
-print(response)
-
-mysocket.send(str.encode('quit'))
-
+    elif(command == 'quit'):
+        mysocket.send(str.encode('quit'))
 
 mysocket.close()
